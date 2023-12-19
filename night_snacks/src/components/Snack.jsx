6e295d381b44d6977b7aa10ab01cbb76f92669a9
arim83 <jorammercado@pursuit.org>
2023-12-19 11:33:43 -0500

    basic index page added

--- night_snacks/src/components/Snack.jsx
+++ night_snacks/src/components/Snack.jsx
@@ -4,7 +4,24 @@ import React from "react"
 export default function Snack({ snack }) {
     return (
         <tr>
+            <td>{snack.is_favorite?`\u2730`:""}</td>
+            <td>
+                <Link to={`/snacks/${snack.resource_id}`}>{snack.name}</Link>
+            </td>
+            <td>{snack.category}</td>
+            <td>{snack.calories}</td>
+            <td>{snack.rating}</td>
+            <td>{snack.creation_date}</td>
 
+            <td>
+                <a href={snack.image} target="_blank" rel="noopener noreferrer">
+                    <img
+                        src={snack.image}
+                        alt={`Image of ${snack.name}`}
+                        style={{ height: "50px" }}
+                    />
+                </a>
+            </td>
         </tr>
     )
 }
--- night_snacks/src/components/SnackList.jsx
+++ night_snacks/src/components/SnackList.jsx
@@ -1,13 +1,118 @@
-import React, { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-const API = import.meta.env.VITE_API_URL
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Snack from "./Snack";
+
+const API = import.meta.env.VITE_API_URL;
 
 export default function SnackList() {
-    
+    const [allSnacks, setAllSnacks] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [snacksPerPage, setSnacksPerPage] = useState(10);
+    const [snacksOrder, setSnacksOrder] = useState(false);
+    const navigate = useNavigate();
+
+    const changeSnackOrder = () => {
+        if (snacksOrder === false) {
+            setSnacksOrder(true)
+            const newOrder = allSnacks.sort((a, b) => {
+                if (a.name.toLowerCase() < b.name.toLowerCase())
+                    return -1
+                else if (a.name.toLowerCase() > b.name.toLowerCase())
+                    return 1
+                else
+                    return 0
+            })
+            setAllSnacks(newOrder)
+        }
+        else {
+            setSnacksOrder(false)
+            const newOrder = allSnacks.sort((b, a) => {
+                if (a.name.toLowerCase() < b.name.toLowerCase())
+                    return -1
+                else if (a.name.toLowerCase() > b.name.toLowerCase())
+                    return 1
+                else
+                    return 0
+            })
+            setAllSnacks(newOrder)
+        }
+    }
+
+    const handleSortSnacks = event => {
+        event.preventDefault()
+        changeSnackOrder()
+    }
+
+    useEffect(() => {
+        fetch(`${API}/snacks`)
+            .then((response) => response.json())
+            .then((snacks) => {
+                setAllSnacks(snacks);
+            })
+            .catch((error) => {
+                console.error("Error fetching data: ", error);
+            });
+    }, []);
+
+    const indexOfLastSnack = currentPage * snacksPerPage;
+    const indexOfFirstSnack = indexOfLastSnack - snacksPerPage;
+    const currentSnacks = allSnacks.slice(indexOfFirstSnack, indexOfLastSnack);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
-        <div className="snacks">
-            
+        <div className="snacks-container">
+            <section>
+                <Table className="edit-table" striped bordered hover>
+                    <thead>
+                        <tr className="table-row">
+                            <th className="favorite">{`\u2730`}</th>
+                            <th className="name">
+                                <Button className="atlBtnColor-sort btn-secondary btn-sm" onClick={handleSortSnacks}>
+                                    Snack Name {` \u21f3`}
+                                </Button>
+                            </th>
+                            <th >Category</th>
+                            <th >Calories</th>
+                            <th >Rating</th>
+                            <th >Date Created </th>
+                            <th >Image</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {currentSnacks.map((snack) => {
+                            return (
+                                <Snack
+                                    key={snack.resource_id}
+                                    snack={snack}
+                                />
+                            );
+                        })}
+                    </tbody>
+                </Table>
+                <div className="snack-container-pagination">
+                    <Button
+                        className="btn btn-secondary"
+                        onClick={() => paginate(currentPage - 1)}
+                        disabled={currentPage === 1}
+                        variant="primary"
+                    >
+                        Previous
+                    </Button>
+                    <span>Page {currentPage}</span>
+                    <Button
+                        className="btn btn-secondary"
+                        onClick={() => paginate(currentPage + 1)}
+                        disabled={indexOfLastSnack >= allSnacks.length}
+                        variant="primary"
+                    >
+                        Next
+                    </Button>
+                </div>
+            </section>
         </div>
-    )
+    );
 }
